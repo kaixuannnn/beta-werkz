24305a1e3e7982fe0bc09c85fbd32d3e48a36dfa
kaixuan <kaixuant@yahoo.com>
2023-08-27 15:01:42 +0800

    update edit profile css

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -11,7 +11,7 @@ const EditProfilePage = () => {
       <h4>New Profile</h4>
       <div className={classes.grid}>
         <AppInput className={classes['item-1']} placeholder='Enter Username' title='Name'></AppInput>
-        <AppInput className={classes['item-2']} placeholder='Enter number' title='Enter Number'></AppInput>
+        <AppInput className={classes['item-2']} placeholder='Enter number' title='Age'></AppInput>
         <AppSelect className={classes['item-3']} />
         <AppInput className={classes['item-4']} placeholder='Enter email address' title='Email'></AppInput>
       </div>
